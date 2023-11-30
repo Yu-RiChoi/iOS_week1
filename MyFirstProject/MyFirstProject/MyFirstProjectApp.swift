f454a2c9be27da6da6be6283459e4919dd19d621
Yu-RiChoi <116857758+Yu-RiChoi@users.noreply.github.com>
2023-11-30 21:03:01 +0900

    Add files via upload

---
+++ MyFirstProject/MyFirstProject/MyFirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstProjectApp.swift
+//  MyFirstProject
+//
+//  Created by 최유리 on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
