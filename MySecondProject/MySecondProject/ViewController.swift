a15c18cc324c8b24bfea31c2f521c5dfce7d642d
Yu-RiChoi <116857758+Yu-RiChoi@users.noreply.github.com>
2023-11-30 21:03:27 +0900

    Add files via upload

---
+++ MySecondProject/MySecondProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySecondProject
+//
+//  Created by 최유리 on 11/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
